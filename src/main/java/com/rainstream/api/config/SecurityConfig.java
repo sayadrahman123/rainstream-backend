407bec13a4b1be36982f66d99165894e74a1c070
abdus <abdusrahman64@gmail.com>
2025-12-14 20:09:09 +0530

    config: Updated SecurityConfig to allow public access to movies

--- src/main/java/com/rainstream/api/config/SecurityConfig.java
+++ src/main/java/com/rainstream/api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.rainstream.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,6 +33,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // <--- Enable CORS
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll() // Open login/register
+                        .requestMatchers(HttpMethod.GET, "/api/v1/movies/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider);
