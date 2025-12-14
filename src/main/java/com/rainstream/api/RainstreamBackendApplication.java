8d88942f69b091f4c80c01944ed247e26b2163ed
abdus <abdusrahman64@gmail.com>
2025-12-14 13:42:47 +0530

    Initial commit: Project setup with Spring Boot and MySQL configuration

---
+++ src/main/java/com/rainstream/api/RainstreamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rainstream.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainstreamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainstreamBackendApplication.class, args);
+    }
+
+}
